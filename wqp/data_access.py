7b57b22ca18f76a8b9bc0fdd2e238762269b68c5
Yann <etrillard.y22@gmail.com>
2020-07-31 14:42:32 +0200

    hotfix

--- wqp/data_access.py
+++ wqp/data_access.py
@@ -1,19 +1,24 @@
 from sklearn.model_selection import train_test_split
-from typing import Optional, Tuple, Dict
 import pandas as pd
 
 
-def fetch_csv_data(url: str, separator: Optional[str]) -> pd.DataFrame:
+def fetch_csv_data(url, separator):
     """
     This functions fetch the CSV data from a given path (or url) and returns a Pandas DataFrame.
     :param url: a string containing the address of the data (local path, url ...)
     :param separator: an optional separator to override the default separator (comma)
     :return: a Pandas Dataframe containing the loaded data
     """
-    raise NotImplementedError()
+    try:
+        if separator:
+            return pd.read_csv(url, sep=separator)
+        else:
+            return pd.read_csv(url)
+    except Exception as e:
+        raise Exception(f'Error while fetching data at url {url}: {e}')
 
-def build_train_test_sets(data: pd.DataFrame, label_col: str, train_size: float) -> \
-        Dict[str, Tuple[pd.DataFrame, pd.DataFrame]]:
+
+def build_train_test_sets(data, label_col, train_size):
     """
     A function to split the data into training and test sets.
 
@@ -22,7 +27,15 @@ def build_train_test_sets(data: pd.DataFrame, label_col: str, train_size: float)
     :param train_size: flaot. The fraction of the whole dataset used for training.
     :return: a Dictionary of key (string) - value (tuple of pandas dataframes) containing training and test data.
     Dictionary keys:
-        - train: contains (train_x, train_y)
-        - test: contains (test_x, test_y
+        - train: contains (X_train, y_train)
+        - test: contains (X_test, y_test)
     """
-    raise NotImplementedError()
+    try:
+        X = data.drop(label_col, axis=1)
+        y = data[label_col]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size)
+
+        return dict(train=(X_train, y_train), test=(X_test, y_test))
+
+    except Exception as e:
+        raise Exception(f'Error while splitting data: {e}')
